0e89cea6220409dc8962cf14a8475e0457687f66
marktmarino76 <marktmarino76@gmail.com>
2023-08-20 12:01:42 -0400

    Delete products from HTML when putting delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,7 +13,7 @@ cart.forEach((cartItem) => {
     }
   });
   cartSummaryHTML += `          
-<div class="cart-item-container">
+<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
 <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
 <div class="cart-item-details-grid">
@@ -96,5 +96,9 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     //Step1: remove the product form the cart
     removeFromCart(productId);
     //Step2: update the HTML and remove the product from the page
+    let container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
