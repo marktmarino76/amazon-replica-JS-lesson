1b715ea619d670bb07d763aaebe11d3f28f3d95d
marktmarino76 <marktmarino76@gmail.com>
2023-08-20 12:07:39 -0400

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,13 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
 
+if (!cart) {
+  cart = [];
+}
+
+function saveToStorage() {
+  //Note: local storage can only save strings
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 export function addToCart(productId) {
   let matchingItem;
 
@@ -26,6 +25,7 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -40,4 +40,5 @@ export function removeFromCart(productId) {
   });
   //Step 4: Re-assigned original cart array to newCart array
   cart = newCart;
+  saveToStorage();
 }
